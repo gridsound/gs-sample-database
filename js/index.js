ab8bdf1272c675e107fc1f27ccbb5eb12af67779
Timer91 <marais.jeremie@gmail.com>
2017-07-18 21:38:12 +0200

    Searching switch between page1 and page2, href keeping hash if exist and fill input

--- js/index.js
+++ js/index.js
@@ -8,6 +8,46 @@ for ( i = 0 ; i < 10 ; i++ ) {
 	uiBlock[ i ].datatype( "buffer" );
 }
 
+/*  -----------------  */
+
+appChangeView = function() {
+	var elApp = document.getElementById( "app" );
+
+	if ( window.location.hash ) {
+		elApp.classList.remove( "page1" );
+		elApp.classList.add( "page2" );
+		fillInput();
+	} else {
+		elApp.classList.remove( "page2" );
+		elApp.classList.add( "page1" );
+	}
+}
+
+fillInput = function() {
+	var
+		hash = window.location.hash,
+		elInput = document.querySelector( "input" )
+	;
+
+	if ( hash ){
+		q = hash.replace( /^\+/g, " " ).replace( /\/$/g, "" )
+		elInput.value = q.substring( 1 );
+	}
+}
+
+init = function() {
+	appChangeView();
+}
+
+init();
+
+window.onhashchange = appChangeView;
+
+document.querySelector( "form" ).onsubmit = function( e ){
+	window.location.hash = `#${this.q.value.trim().replace( /\s+/g, "+" )}/`;
+	return false;
+};
+
 /** /
 fetch( "https://gridsound.github.io/assets/demo-files/120bpm-4s.wav" )
 	.then( res => res.arrayBuffer() )
