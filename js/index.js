5dc60eb48f1dd97859dd7021d5140b05808380ca
Timer91 <marais.jeremie@gmail.com>
2017-07-20 23:48:35 +0200

    keywords : bug with logs

--- js/index.js
+++ js/index.js
@@ -13,12 +13,13 @@ function appChangeView() {
 	var clApp = document.getElementById( "app" ).classList,
 		query = isValidHash( location.hash );
 
+	lg( "appChangeView" );
 	clApp.toggle( "page1", !query );
 	clApp.toggle( "page2", query );
-	setInput();
 }
 
 function setInput() {
+	lg( "setInput" );
 	document.querySelector( "input" ).value =
 		isValidHash( location.hash )
 			? location.hash.replace( /\+/g, " " ).substring( 2 )
@@ -29,10 +30,25 @@ function isValidHash( hash ) {
 	return hash && hash[ 0 ] === "#" && hash[ 1 ] === "/";
 }
 
+function keywordsOnclick() {
+	var elsKeywords = document.getElementById( "keywords" ).children,
+		i = 0;
+
+	for ( ; i < elsKeywords.length ; ++i ) {
+		elsKeywords[ i ].onclick = function() {
+			document.querySelector( "input" ).value = this.textContent;
+			document.querySelector( "form" ).onsubmit();
+		}
+	};
+}
+
 window.onhashchange = appChangeView;
-document.querySelector( "form" ).onsubmit = function( e ){
+document.querySelector( "form" ).onsubmit = function() {
+	lg( "onsubmit" );
 	location.hash = "#/" + this.q.value.trim().replace( /\s+/g, "+" );
 	return false;
 };
 
 appChangeView();
+setInput();
+keywordsOnclick();
